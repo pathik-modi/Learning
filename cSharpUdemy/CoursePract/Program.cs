07e96dc0943d0d9290fa2421c9ad2324e1d124f0
Pathik Modi <modipathik95@gmail.com>
2023-12-17 17:08:39 +1300

    Add udemy practice file

---
+++ cSharpUdemy/CoursePract/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CoursePract;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var number = 2;
+        var count = 10;
+        var totalPrice = 20.95f;
+        var character = 'A';
+        var firstName = "Mosh";
+        var isWorking = false;
+        Console.WriteLine(number);
+        System.Console.WriteLine(count);
+        System.Console.WriteLine(totalPrice);
+        System.Console.WriteLine(character);
+        System.Console.WriteLine(firstName);
+        System.Console.WriteLine(isWorking);
+    }
+}
