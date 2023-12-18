6f57b018f999d8a2223988acd1cd238a719100f4
Pathik Modi <modipathik95@gmail.com>
2023-12-18 18:01:45 +1300

    Add object equivalent in C#

---
+++ cSharpUdemy/ArrayPract/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArrayPract;
+
+  public enum ShippingMethod
+  {
+    RegularAirMail = 1,
+    RegisteredAirMail = 2,
+    Express = 3
+  } 
+  class Program
+  {
+      static void Main(string[] args)
+      {
+        var method = ShippingMethod.Express;
+        System.Console.WriteLine((int)method);
+        var methodId = 3;
+        System.Console.WriteLine((ShippingMethod)methodId);
+      }
+  }
+
+
+
+
