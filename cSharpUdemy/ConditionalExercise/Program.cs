5df11a0008e6acb3cff79b76619073de51549a1f
Pathik Modi <modipathik95@gmail.com>
2023-12-19 13:32:58 +1300

    Add task 2 in conditional operator exercise

---
+++ cSharpUdemy/ConditionalExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConditionalExercise;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      // Exc 1. Take user input for a number and check if its between 1 & 10
+        Console.WriteLine("1. Enter a number");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number > 0 && number < 11) {
+          System.Console.WriteLine("Result: Valid");
+        }
+        else {
+          System.Console.WriteLine("Result: Invalid");
+        }
+
+        // Exc 2. enter two numbers and display the higer number
+        System.Console.WriteLine("\n2. Compare two numbers here\nEnter first number below:");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("Enter second number below:");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        System.Console.WriteLine((number1 > number2) ? $"Result: {number1} is higher." : $"Result: {number2} is higher.");
+    }
+}
